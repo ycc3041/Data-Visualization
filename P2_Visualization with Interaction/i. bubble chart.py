40c0e4c843b46b3138cea108cd79f849b51b7639
Zoey Chen <44735519+ycc3041@users.noreply.github.com>
2018-11-04 00:28:55 -0400

    Update i. bubble chart.py

--- P2_Visualization with Interaction/i. bubble chart.py
+++ P2_Visualization with Interaction/i. bubble chart.py
@@ -1,41 +1,15 @@
-
-# coding: utf-8
-
-# # Programming Project 2: Interaction
-# ● Due data: 9/20/2018
-
-# In[2]:
-
 import pandas as pd
 from plotly.offline import plot
+import plotly.plotly as py
+import plotly.graph_objs as go
 
-# In[3]:
 
 df = pd.read_csv('factbook.csv')
 df.head()
 
 
-# In[4]:
-
-df.columns
-
-
-# ## Task 1: Bubble Chart
-# - We saw in class a short clip of the famous TED Talk that Hans Rosling gave in 2006 in which he visualized global health statistics. For this first task, I am asking you to implement a basic bubble chart visualization, in other words a scatter plot in which individual data points are plotted as circles whose radius and color vary as a function of two variables (in addition to the two variables that control the (x,y) position of each circle). 
-# - To demonstrate your bubble chart implementation, you will create following visualization: The x coordinate corresponds to 'GDP per capita', the y coordinate corresponds to 'Life expectancy at birth', the radius corresponds to 'Population', and the color corresponds to 'Birth rate'. Make sure to select scales, ranges, and color maps appropriately.
-
-# In[5]:
-
-import plotly.plotly as py
-import plotly.graph_objs as go
-
-
-# In[6]:
-
 df[['Country', 'GDP per capita', 'Population','Birth rate']].head()
 
-
-# In[7]:
 
 df['Text'] = 'Country: ' + df['Country'] + '<br>' +     'GDP per capita: ' + df['GDP per capita'].astype(str) + '<br>'  +     'Population: ' + df['Population'].astype(str) + '<br>' +     'Birth rate: ' + df['Birth rate'].astype(str)
 
